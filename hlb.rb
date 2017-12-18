04dc46bf4ad32c11570e6a7d20bcc413bb7e677a
niboshi <niboshiporipori@gmail.com>
2017-12-18 22:30:37 +0900

    Update hlb.rb

--- hlb.rb
+++ hlb.rb
@@ -1,8 +1,8 @@
 class Hlb < Formula
-  desc "CLI that provides a unified interface to multiple git repository hosting services"
+  desc "CLI that provides a unified & interactive interface to multiple git repository hosting services"
   homepage "https://github.com/mpppk/hlb"
-  url "https://github.com/mpppk/hlb/releases/download/v0.0.1/ghr_v0.0.1_darwin_amd64.zip"
-  sha256 "cad0c0e29b982ba1d4ee9947e64e74449c660e8151d4b2a06471208b148acdea"
+  url "https://github.com/mpppk/hlb/releases/download/v0.0.2/ghr_v0.0.2_darwin_amd64.zip"
+  sha256 "940227e1c43df50e0bc7ef1bfd2efd2c6edbddc67a68325338700e6d5b5e4638"
 
   def install
     bin.install 'hlb'
