8b12ceb5e697d7f39142dd4e764fdfc43883bf7e
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-16 21:21:19 +0900

    Brew formula update for hlb version v0.0.3

--- hlb.rb
+++ hlb.rb
@@ -1,9 +1,9 @@
 class Hlb < Formula
   desc "CLI that provides unified & interactive interface to multiple git repository hosting services"
   homepage "https://github.com/mpppk/hlb"
-  url "https://github.com/mpppk/hlb/releases/download/v0.0.3/hlb_0.0.3_Darwin_x86_64.tar.gz"
+  url "https://github.com/mpppk/hlb/releases/download/v0.0.3/hlb_0.0.3_darwin_amd64.tar.gz"
   version "0.0.3"
-  sha256 "37e666a4eb1fe6fb5605c2d1fe05fa1235a8572d05b404fcac25d61f17eecee0"
+  sha256 "819e3033f7c25558f2e36aa9e9c325f6653bc32ffaeff2dbd7de3b0ffe1d3f25"
   
   depends_on "peco"
 
